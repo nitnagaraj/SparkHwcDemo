4455555af089235c9479e037460bc0bc0d629b5a
Nithin Nagaraj <nnagaraj@HW15616.local>
2019-11-27 21:05:16 +0530

    adding files

---
+++ src/main/scala/com/hwx/SparkSecureHWCDemo.scala
@@ -0,0 +1,22 @@
+package com.hwx
+
+  import com.hortonworks.hwc.HiveWarehouseSession
+  import org.apache.spark.SparkConf
+  import org.apache.spark.sql.SparkSession
+  import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+  object SparkSecureHWCDemo extends App {
+
+
+  import com.hortonworks.hwc.HiveWarehouseSession
+  val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
+  val hive = HiveWarehouseSession.session(spark).build()
+  hive.execute("show tables").show
+
+  val tname = args(0)
+
+  hive.executeQuery(s"select * from $tname").show()
+
+
+  } 
+
